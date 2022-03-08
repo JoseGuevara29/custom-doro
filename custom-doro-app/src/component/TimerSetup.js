a3312859fd3cc8f8d140d6776fff4a152b31b215
joseguevara29 <guevarabus29@gmail.com>
2022-03-08 13:09:14 -0500

    git ignore testing

--- custom-doro-app/src/component/TimerSetup.js
+++ custom-doro-app/src/component/TimerSetup.js
@@ -3,39 +3,5 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 export default function TimerSetup() {
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "15ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        {" "}
-        <TextField
-          id="filled-number"
-          label="Hours"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-      </div>
-      <div>
-        {" "}
-        <TextField
-          id="filled-number"
-          label="Minutes"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-      </div>
-    </Box>
-  );
+  return <div>testing</div>;
 }
